8b2e4f2c310312b5f0cd235e43bdfc3119b64de2
AhmetImran <iahmetkavras@gmail.com>
2025-07-30 15:40:47 +0300

    Cell creation added

--- Assets/Scripts/Match3/Items/GameFieldBuilder.cs
+++ Assets/Scripts/Match3/Items/GameFieldBuilder.cs
@@ -1,4 +1,5 @@
 using System;
+using MInject.Runtime;
 using UnityEngine;
 
 namespace Match3.Items
@@ -6,15 +7,18 @@ namespace Match3.Items
     public class GameFieldBuilder : MonoBehaviour
     {
         public LevelData _levelData;
+        [SerializeField] private Board board;
         private void Start()
         {
             var loader = new LevelLoader();
             _levelData = loader.LoadLevelData(1);
 
             PrintGrid(_levelData);
+            
+            board.Prepare(_levelData);
         }
         
-        public static void PrintGrid(LevelData data)
+        private static void PrintGrid(LevelData data)
         {
             for (int row = 0; row < data.Rows; row++)
             {
@@ -25,6 +29,12 @@ namespace Match3.Items
                 }
                 Debug.Log(line);
             }
+        }
+
+        [Inject]
+        public void Construct()
+        {
+            Debug.Log("ahmet deneme");
         }
 
     }
